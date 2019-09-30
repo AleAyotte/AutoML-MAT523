a72018dc4856115c70bc3521db43c5a55f0f2a35
AleAyotte <alexandre.ayotte2@usherbrooke.ca>
2019-09-29 22:12:36 -0400

    Some comment as been added to DataGen.py

--- Code/DataGen.py
+++ Code/DataGen.py
@@ -15,11 +15,24 @@ import matplotlib.pyplot as plt
 class DataGenerator:
 
     def __init__(self, train, test, model_name):
+        """
+        Class that generate training and testing sample from sklearn datasets.
+
+        :param train: The training sample length
+        :param test: The testing sample length
+        :param model_name: The name of the model that will be use to generate the samples.
+        """
         self.train_size = train
         self.test_size = test
         self.model = model_name
 
     def generate_data(self, noise):
+        """
+        Generate random training and testing sample according to the model name
+
+        :param noise: The standard deviation of the Gaussian noise added to the data
+        :return: 4 numpy array for training features, training labels, testing features and testing labels respectively
+        """
         if self.model == "half_moon":
             x_train, t_train = make_moons(self.train_size, noise=noise)
             x_test, t_test = make_moons(self.test_size, noise=noise)
