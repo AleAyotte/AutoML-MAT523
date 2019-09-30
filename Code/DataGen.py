f308206266f7d06c7f409f797d7b5dc4700746c5
AleAyotte <alexandre.ayotte2@usherbrooke.ca>
2019-09-29 22:01:02 -0400

    I create DataGen.py to replace DataGenerator.py, the difference is that the new program call sklearn to generate datasets for toy problems.

--- Code/DataGen.py
+++ Code/DataGen.py
@@ -6,7 +6,8 @@
     @Description:       This program generate random data for toy problem that we want to solve with autoML method
 """
 
-import sklearn
+from sklearn.datasets import make_moons
+from sklearn.datasets import make_circles
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -17,3 +18,18 @@ class DataGenerator:
         self.train_size = train
         self.test_size = test
         self.model = model_name
+
+    def generate_data(self, noise):
+        if self.model == "half_moon":
+            x_train, t_train = make_moons(self.train_size, noise=noise)
+            x_test, t_test = make_moons(self.test_size, noise=noise)
+
+        elif self.model == "circles":
+            x_train, t_train = make_circles(self.train_size, noise=noise)
+            x_test, t_test = make_circles(self.test_size, noise=noise)
+
+        else:
+            print("Model: {} does not exist in this program".format(self.model))
+            return -1   # Return -1 cause the
+
+        return x_train, t_train, x_test, t_test
