00944a7c1dca87143b7557615981bba4c24f7661
Rayn2402 <nicolas.raymon2@usherbrooke.ca>
2019-10-01 09:32:41 -0400

    Creation of HPtuner, coding of constructor and functions setting hyperparameters search space

---
+++ Code/HPtuner.py
@@ -0,0 +1,60 @@
+"""
+    @file:              HPtuner.py
+    @Author:            Nicolas Raymond
+    @Creation Date:     01/10/2019
+    @Last modification: 01/10/2019
+    @Description:       This file provide all functions linked to hyperparameters optimization methods
+"""
+
+
+
+class HPtuner:
+
+    def __init__(self, model, method):
+
+        """
+        Class that generate an automatic hyperparameter tuner for the model specified
+
+        :param model: Model on which we want to optimize hyperparameters {SVM, MLP} # Work in progress
+        :param method: Method of optimization to use {'grid_search', 'random_search'} # Work in progress
+        """
+
+        self.model = model
+        self.method = method
+
+
+    def set_search_space(self, hp_search_space_dict):
+
+        """
+
+        Function that defines hyperparameter's possible values (or distribution) for all hyperparameters in our model attribute
+
+        :param hp_search_space_dict: Dictionary specifing hyperparameters to tune and search space associate to each of them.
+                                     Search space must be a list of values or a distribution.
+
+        :return: Change hyperparameter space in our model attribute
+
+        """
+
+        for hp in hp_search_space_dict:
+            self.set_single_hp_space(hp, hp_search_space_dict[hp])
+
+
+    def set_single_hp_space(self, hyperparameter, space):
+
+        """
+        Function that defines hyperparameter's possible values (or distribution) in our model attribute
+
+        :param hyperparameter: Name of the hyperparameter
+        :param space: List of values or statistical distribution
+
+        :return: Change value associate with the hyperparameter in our model attribute HP_space dictionary
+
+        """
+
+        if hyperparameter in self.model.HP_space:
+            self.model.HP_space[hyperparameter] = space
+
+        else:
+            raise Exception('No such hyperparameter "{}" in our model'.format(hyperparameter))
+
