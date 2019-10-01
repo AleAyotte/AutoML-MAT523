eccc2047ee82af70245532b682cf5ab4f9f6630f
Rayn2402 <nicolas.raymon2@usherbrooke.ca>
2019-09-30 22:41:46 -0400

    New class SVM (child of model)

--- Code/Model.py
+++ Code/Model.py
@@ -8,6 +8,7 @@
 """
 
 import numpy as np
+import sklearn as sk
 
 class Model:
 
@@ -19,28 +20,31 @@ class Model:
         :param HP_Dict: Dictionary containing all hyperparameters of the model
 
         """
+        hyperparams = HP_Dict
 
-        Hyperparam = HP_Dict
 
-
-    def train(self):
+    def train(self, X_train, t_train):
 
         """
         Train our model
 
         Note that it wil be override by the children's classes
+
+        :param X_train: NxD numpy array of observations {N : nb of obs, D : nb of dimensions}
+        :param t_train: Nx1 numpy array of classes associated with each observations
+
         """
 
         return NotImplementedError
 
 
-    def predict(self, x):
+    def predict(self, X):
 
         """
         Predict the class for an observation x
 
-        :param x: Value (float) or 1-d numpy array of values
-        :return: Class (integer)
+        :param X: NxD numpy array of observations {N : nb of obs, D : nb of dimensions}
+        :return: Nx1 numpy array of classes predicted for each observation
         """
 
         return NotImplementedError
@@ -48,14 +52,57 @@ class Model:
     def accuracy(self, X, T):
 
         """
-
-        :param X: NxD numpy array of observations (N : nb of obs, D : nb of dimensions)
-        :param T: Nx1 numpy array
+        :param X: NxD numpy array of observations {N : nb of obs, D : nb of dimensions}
+        :param T: Nx1 numpy array of classes associated with each observation
         :return: Good classification rate
 
         """
-        predictions = np.array([self.predict(x) for x in X]) # Nx1 array containing predictions
+        predictions = self.predict(X)
         diff = T - predictions
 
         return ((diff == 0).sum())/len(diff) # (Nb of good predictions / nb of predictions)
+
+
+class SVM(Model):
+
+    def __init__(self, C=1.0, kernel="rbf", degree=3, gamma='auto_deprecated', coef0=0.0):
+
+        """
+        Class that generate a support vector machine
+
+        Take a look at https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html#sklearn.svm.SVC for
+        more informations on hyperparameters
+
+        :param C: Penalty parameter C of the error term
+        :param kernel: Kernel type to be used in the algorithm. It must be one of ‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’ or a callable
+        :param degree: Degree of the polynomial kernel function (‘poly’). Ignored by all other kernels
+        :param gamma: Kernel coefficient for ‘rbf’, ‘poly’ and ‘sigmoid’
+        :param coef0: Independent term in kernel function. It is only significant in ‘poly’ and ‘sigmoid’.
+        """
+
+        model = sk.svm.SVC(C, kernel, degree, gamma, coef0)
+        super().__init__({'C':C, 'kernel':kernel, 'degree':degree, 'gamma':gamma, 'coef0':coef0})
+
+
+    def train(self, X_train, t_train):
+
+        """
+        Train our model
+
+        :param X_train: NxD numpy array of the observations of the training set
+        :param t_train: Nx1 numpy array classes associated with each observations in the training set
+        """
+
+        self.model.fit(X_train, t_train)
+
+
+    def predict(self, X):
+
+        """
+        :param X: NxD numpy array of observations {N : nb of obs, D : nb of dimensions}
+        :return: Nx1 numpy array classes predicted for each observation
+
+        """
+
+        return self.model.predict(X)
 
