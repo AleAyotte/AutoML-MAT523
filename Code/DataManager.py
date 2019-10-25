8addb0748b304030a246c3d796d44da568562091
AleAyotte <alexandre.ayotte2@usherbrooke.ca>
2019-10-25 17:28:25 -0400

    Datamanager correction in load_mnist method

--- Code/DataManager.py
+++ Code/DataManager.py
@@ -156,7 +156,7 @@ def load_mnist():
     """
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-    testset = torchvision.datasets.MNIST(root='./data', train=Test, download=True, transform=transform)
+    testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 
     return trainset, testset
 
