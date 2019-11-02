5d072e9de3cbaf6109217a9a5d857aa7fda4f699
AleAyotte <alexandre.ayotte2@usherbrooke.ca>
2019-11-02 18:05:22 -0400

    Create_dataloader method has now a new parameters: shuffle. To control if the dataloader shuffle the data at each epoch

--- Code/DataManager.py
+++ Code/DataManager.py
@@ -101,19 +101,20 @@ class DataGenerator:
         return x_train, t_train, x_test, t_test
 
 
-def create_dataloader(features, labels, b_size):
+def create_dataloader(features, labels, b_size, shuffle=False):
     """
     Transform a n dimensional numpy array of features and a numpy array of labels into ONE data loader
 
     :param features: A n dimensional numpy array that contain features of each data points
     :param labels: A numpy array that represent the correspondent labels of each data points
     :param b_size: Batch size as integer
-    :return: A dataloader that contain
+    :param shuffle: Do we want to shuffle the data at each epoch
+    :return: A dataloader that contain given features and labels.
     """
     tensor_x = torch.tensor(features, dtype=torch.float)
     tensor_y = torch.tensor(labels, dtype=torch.long)
     dt = utils.TensorDataset(tensor_x, tensor_y)
-    dt_load = utils.DataLoader(dt, batch_size=b_size, shuffle=True, drop_last=True)
+    dt_load = utils.DataLoader(dt, batch_size=b_size, shuffle=shuffle, drop_last=True)
 
     return dt_load
 
