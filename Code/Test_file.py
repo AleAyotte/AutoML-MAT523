7b65940f8eba5ab07afe7192a9266a5105e831e6
Rayn2402 <nicolas.raymon2@usherbrooke.ca>
2019-11-14 23:10:22 -0500

    Update in test_file (new lines to save results)

--- Code/Test_file.py
+++ Code/Test_file.py
@@ -11,7 +11,8 @@ def main():
 
     # We generate data for our tests
     dgen = dm.DataGenerator(500, 500, "nSpiral")
-    x_train, t_train, x_test, t_test = dgen.generate_data(0.35)
+    noise = 0.28
+    x_train, t_train, x_test, t_test = dgen.generate_data(0.28, 5)
     dm.plot_data(x_train, t_train)
 
     """
@@ -69,32 +70,36 @@ def main():
 
         # We optimize hyper-parameters with random search
         mlp_tuner = HPtuner(mlp, 'tpe', test_default_hyperparam=True)
-        mlp_tuner.set_search_space({'alpha': ContinuousDomain(0, 1),
-                                    'learning_rate_init': ContinuousDomain(-6, 0, log_scaled=True)})
+        mlp_tuner.set_search_space({'alpha': ContinuousDomain(-6, 0, log_scaled=True),
+                                    'learning_rate_init': ContinuousDomain(-6, 0, log_scaled=True),
+                                    'batch_size': DiscreteDomain(linspace(50, 500, 10, dtype=int))})
 
-        results = mlp_tuner.tune(x_train, t_train, n_evals=100, nb_cross_validation=2)
+        results = mlp_tuner.tune(x_train, t_train, n_evals=150, nb_cross_validation=2)
 
         # We look at the new results
         results.plot_accuracy_history()
         results.plot_accuracy_history(best_accuracy=True)
         mlp.plot_data(x_test, t_test)
+        results.save_all_results('Dummy_MLP_5_20', dgen.model, dgen.train_size, noise)
 
         # ---------------------------------------------------------------------------------- #
 
         # We generate another set of data to test an svm with a polynomial kernel
         dgen = dm.DataGenerator(500, 500, "circles")
-        x_train, t_train, x_test, t_test = dgen.generate_data(0.08)
+        noise = 0.05
+        x_train, t_train, x_test, t_test = dgen.generate_data(noise)
         dm.plot_data(x_train, t_train)
 
         # We do the same exercice for an svm
         svm = mod.SVM(kernel='poly')
         svm_tuner = HPtuner(svm, 'tpe', test_default_hyperparam=True)
-        svm_tuner.set_search_space({'C': ContinuousDomain(-6, 0, log_scaled=True),
+        svm_tuner.set_search_space({'C': ContinuousDomain(-2, 0, log_scaled=True),
                                     'degree': DiscreteDomain([1, 2, 3, 4, 5, 6])})
-        results = svm_tuner.tune(x_train, t_train, n_evals=100, nb_cross_validation=3)
+        results = svm_tuner.tune(x_train, t_train, n_evals=500, nb_cross_validation=4)
         results.plot_accuracy_history()
         results.plot_accuracy_history(best_accuracy=True)
         svm.plot_data(x_test, t_test)
+        results.save_all_results('Dummy_polynomial500', dgen.model, dgen.train_size, noise)
 
     """
     
@@ -105,19 +110,21 @@ def main():
     if test == 'gaussian_process':
 
         # We generate an MLP to classify our data (5 hidden layers of 20 neurons)
-        mlp = mod.MLP((20, 20, 20, 20, 20), max_iter=1000, learning_rate_init=3, alpha=2)
+        mlp = mod.MLP((20, 20, 20, 20, 20), max_iter=1000)
 
         # We optimize hyper-parameters with random search
         mlp_tuner = HPtuner(mlp, 'gaussian_process', test_default_hyperparam=True)
-        mlp_tuner.set_search_space({'alpha': ContinuousDomain(0, 1),
-                                    'learning_rate_init': ContinuousDomain(-6, 0, log_scaled=True)})
+        mlp_tuner.set_search_space({'alpha': ContinuousDomain(-6, 0, log_scaled=True),
+                                    'learning_rate_init': ContinuousDomain(-6, 0, log_scaled=True),
+                                    'batch_size': DiscreteDomain(list(linspace(50, 500, 10, dtype=int)))})
 
-        results = mlp_tuner.tune(x_train, t_train, n_evals=15)
+        results = mlp_tuner.tune(x_train, t_train, n_evals=50, nb_cross_validation=2)
 
         # We look at the tuning results
         results.plot_accuracy_history()
         results.plot_accuracy_history(best_accuracy=True)
         mlp.plot_data(x_test, t_test)
+        results.save_all_results('Dummy_MLP_5_20_500', dgen.model, dgen.train_size, noise)
 
 
 if __name__ == '__main__':
