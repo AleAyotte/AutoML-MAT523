5db7c8f595bcf10e83617505a67193fc79eb993e
Alexandre Ayotte <alexandre.ayotte2@Usherbrooke.ca>
2020-07-13 13:52:41 -0400

    Noisy Expectation Improvement experiment added

--- Code/Experiments/Experiment_frame.py
+++ Code/Experiments/Experiment_frame.py
@@ -80,7 +80,7 @@ def run_experiment(model, experiment_title, search_space, total_budget, max_budg
     """
     # Simulated Annealing
     """
-
+    """
     print('\n\n SIMULATED ANNEALING \n\n')
 
     # We do a deep copy of our MLP for the test, initialize a tuner with tpe method and set our search space
@@ -100,7 +100,7 @@ def run_experiment(model, experiment_title, search_space, total_budget, max_budg
     """
     # Standard GP with EI acquisition function
     """
-
+    """
     print('\n\n GP WITH EI \n\n')
 
     # We do a deep copy of our MLP for the test, initialize a tuner with the standard GP method and set our search space
@@ -118,11 +118,11 @@ def run_experiment(model, experiment_title, search_space, total_budget, max_budg
     # We save the results
     GP_results.save_all_results(results_path, experiment_title, dataset_name,
                                 train_size, model_for_GP.score(X=x_test, t=t_test, dtset=dtset_test), noise=noise)
-
+    """
     """
     # Standard GP with MPI acquisition function
     """
-
+    """
     print('\n\n GP WITH MPI \n\n')
 
     # We do a deep copy of our MLP for the test, initialize a tuner with the standard GP method and set our search space
@@ -142,10 +142,33 @@ def run_experiment(model, experiment_title, search_space, total_budget, max_budg
     GP_results2.save_all_results(results_path, experiment_title, dataset_name,
                                  train_size, model_for_GP2.score(X=x_test, t=t_test, dtset=dtset_test), noise=noise)
     """
+    
+        """
+    # Standard GP with NEI acquisition function
+    """
+
+    print('\n\n GP WITH MPI \n\n')
+
+    # We do a deep copy of our MLP for the test, initialize a tuner with the standard GP method and set our search space
+    model_for_GP2 = pickle.loads(save)
+    GP_tuner2 = HPtuner(model_for_GP2, 'gaussian_process', total_budget=total_budget,
+                        max_budget_per_config=max_budget_per_config)
+
+    GP_tuner2.set_search_space(search_space)
+
+    # We execute the tuning using default parameter for GP except MPI acquisition
+    # ('GP' as method type, 5 initial points to evaluate before the beginning and 'MPI' acquisition)
+    GP_results2 = GP_tuner2.tune(X=x_train, t=t_train, dtset=dtset_train,
+                                 nb_cross_validation=nb_cross_validation, acquisition_function='NEI',
+                                 valid_size=valid_size)
+
+    # We save the results
+    GP_results2.save_all_results(results_path, experiment_title, dataset_name,
+                                 train_size, model_for_GP2.score(X=x_test, t=t_test, dtset=dtset_test), noise=noise)
     """
     # Hyperband
     """
-
+    """
     print('\n\n HYPERBAND \n\n')
 
     # We do a deep copy of our MLP for the test, initialize a tuner with the standard GP method and set our search space
@@ -162,11 +185,11 @@ def run_experiment(model, experiment_title, search_space, total_budget, max_budg
 
     hb_results.save_all_results(results_path, experiment_title, dataset_name,
                                 train_size, model_for_hb.score(X=x_test, t=t_test, dtset=dtset_test), noise=noise)
-
+    """
     """
     # BOHB
     """
-
+    """
     print('\n\n BOHB \n\n')
 
     # We do a deep copy of our MLP for the test, initialize a tuner with the standard GP method and set our search space
@@ -183,6 +206,7 @@ def run_experiment(model, experiment_title, search_space, total_budget, max_budg
 
     bohb_results.save_all_results(results_path, experiment_title, dataset_name,
                                   train_size, model_for_bohb.score(X=x_test, t=t_test, dtset=dtset_test), noise=noise)
+    """
     """
     if grid_search_space is not None:
 
